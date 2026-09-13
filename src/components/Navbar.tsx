a16b32ab56bb045f9c649f20a1b14c405f972c04
R-pradeep2005 <pradeep.work@hotmail.com>
2026-09-13 11:24:40 +0530

    updated Repo Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
       
 
         <a
-          href="https://github.com/R-pradeep2005/TalentGH"
+          href="https://github.com/R-pradeep2005/TalentGH-Java-"
           target="_blank"
           rel="noreferrer"
           className="flex items-center gap-2 rounded-xl border border-white/10 bg-white/5 px-4 py-2 text-sm font-medium text-slate-100 backdrop-blur-xl transition hover:border-white/20 hover:bg-white/10"
